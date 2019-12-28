55439d1e1d095f958dd5421b640111c9204caa8a
Fiona <fyeung21@gmail.com>
2019-12-28 03:16:35 -0800

    sketch hero and skills

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -3,7 +3,27 @@ import React from "react"
 const Skills = () => {
     return (
         <div>
-            <h3>Skills displayed here</h3>
+            <h1>skills</h1>
+
+            <div className="skill-grid">
+                <div className="skill-item">
+                    <h2>html</h2>
+                    <p>html, php</p>
+
+                </div>
+                <div className="skill-item">
+                    <h2>Styling</h2>
+                    <p>React, CSS, Sass</p>
+                </div>
+                <div className="skill-item">
+                    <h2>JS</h2>
+                    <p>Node, Express</p>
+                </div>
+                <div className="skill-item">
+                    <h2>Tools/CMS</h2>
+                    <p>Meteor, Gulp, WordPress</p>
+                </div>
+            </div>
         </div>
     )
 }
