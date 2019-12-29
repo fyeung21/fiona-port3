49fcb313a3dba84e91ccb20c05e5f3d55aae8bf3
Fiona <fyeung21@gmail.com>
2019-12-28 18:00:03 -0800

    sketch hero and skills

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -3,7 +3,19 @@ import React from "react"
 const Hero = () => {
     return (
         <div>
-            <h3>Hero displayed here</h3>
+            <h1>Hi there! I'm Fiona Yeung, a full stack developer from Vancouver, BC.</h1>
+            <ul>
+                <li>
+                    <a href="#projects">
+                        projects
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        resume
+                    </a>
+                </li>
+            </ul>
         </div>
     )
 }
