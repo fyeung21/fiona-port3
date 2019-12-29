b26a25c834f4085cbe7ee56044947a5d3dc9b7a0
Fiona <fyeung21@gmail.com>
2019-12-28 18:58:09 -0800

    basic style hero

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -2,21 +2,39 @@ import React from "react"
 
 const Hero = () => {
     return (
-        <div>
+        <div style={{ padding: "11rem 0", maxWidth: "70%" }}>
             <h1>Hi there! I'm Fiona Yeung, a full stack developer from Vancouver, BC.</h1>
-            <ul>
+            <ul style={{ display: "flex", listStyle: "none" }}>
                 <li>
-                    <a href="#projects">
+                    <a href="#projects"
+                        style={{
+                            margin: "0 5px",
+                            textTransform: "capitalize",
+                            textDecoration: "none",
+                            color: "black",
+                            border: "2px solid teal",
+                            borderRadius: "35px",
+                            padding: "10px 20px"
+                        }}>
                         projects
                     </a>
                 </li>
                 <li>
-                    <a href="#">
+                    <a href="#"
+                        style={{
+                            margin: "0 5px",
+                            textTransform: "capitalize",
+                            textDecoration: "none",
+                            color: "black",
+                            border: "2px solid teal",
+                            borderRadius: "35px",
+                            padding: "10px 20px"
+                        }}>
                         resume
                     </a>
                 </li>
             </ul>
-        </div>
+        </div >
     )
 }
 
