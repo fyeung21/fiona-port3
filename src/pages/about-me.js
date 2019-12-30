85af753ea668c54c2284b084a104ab255a32e2d3
Fiona <fyeung21@gmail.com>
2019-12-29 20:18:22 -0800

    basic style aboutme

--- src/pages/about-me.js
+++ src/pages/about-me.js
@@ -1,16 +1,30 @@
 import React from "react"
-import { Link } from "gatsby"
-
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const AboutMe = () => (
   <Layout>
     <SEO title="about me" />
-    <h1>About Me</h1>
-    <p>Welcome to page 2</p>
-    <img src="" loading="lazy" />
-    <Link to="/">Go back to the homepage</Link>
+    <div>
+      <h1 style={{ margin: "3rem 5rem", textAlign: "center" }}>About Me</h1>
+      <div style={{ display: "flex" }}>
+        <p style={{
+          maxWidth: "50%",
+          marginRight: "2rem",
+          borderRadius: "5px",
+          width: "100%",
+          height: "400px",
+          marginTop: "3rem"
+        }}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore nemo omnis harum error ratione
+          delectus
+          dolor quod accusantium voluptatum, hic voluptates optio sunt culpa aspernatur illum suscipit.
+          Molestias,
+          voluptatibus numquam.
+    </p>
+        <img src="../img.png" loading="lazy" alt="me" />
+      </div>
+    </div>
   </Layout>
 )
 
