32628f14e0c24b63e6f65a03abdaf3f7c0548da1
Fiona <fyeung21@gmail.com>
2019-12-29 20:07:39 -0800

    basic style footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,26 +2,47 @@ import React from "react"
 
 const Footer = () => {
     return (
-        <div>
-            <h1>Contact Me!</h1>
-            <div>
-                <h2>fiona yeung</h2>
-                <ul>
-                    <li><i class="far fa-envelope"></i>
-                        <a href="#">
-                            <p>fyeung21@gmail.com</p>
-                        </a>
-                    </li>
+        <div style={{
+            backgroundColor: "darkgrey",
+            borderRadius: "5px"
+        }}>
+            <div style={{
+                display: "flex",
+                justifyContent: "space-between",
+                maxWidth: "1000px",
+                margin: "0 auto",
+                padding: "3rem 0"
+            }}>
+                <h1>Contact Me!</h1>
+                <div style={{
+                }}>
+                    <h2 style={{ textTransform: "capitalize" }}>fiona yeung</h2>
+                    <ul style={{ listStyle: "none" }}>
+                        <li><i class="far fa-envelope"></i>
+                            <a href="#"
+                                style={{ textDecoration: "none", color: "black" }}
+                            >
+                                <p>fyeung21@gmail.com</p>
+                            </a>
+                        </li>
 
-                    <li><i class="fab fa-linkedin-in"></i>
-                        <a href="https://www.linkedin.com/in/fionaa-yeungg/" target="_blank">
-                            <p>linkedin.com/in/fionaa-yeungg</p>
-                        </a></li>
-                    <li><i class="fab fa-github"></i>
-                        <a href="https://github.com/fyeung21" target="_blank">
-                            <p>github.com/fyeung21</p>
-                        </a></li>
-                </ul>
+                        <li><i class="fab fa-linkedin-in"></i>
+                            <a href="https://www.linkedin.com/in/fionaa-yeungg/"
+                                target="_blank"
+                                style={{ textDecoration: "none", color: "black" }}
+                            >
+                                <p>linkedin.com/in/fionaa-yeungg</p>
+                            </a></li>
+
+                        <li><i class="fab fa-github"></i>
+                            <a href="https://github.com/fyeung21"
+                                target="_blank"
+                                style={{ textDecoration: "none", color: "black" }}
+                            >
+                                <p>github.com/fyeung21</p>
+                            </a></li>
+                    </ul>
+                </div>
             </div>
         </div>
     )
