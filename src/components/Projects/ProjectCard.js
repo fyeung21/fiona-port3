b8f7fe3418761f586c88d46f2f7435b0cc46e207
Fiona <fyeung21@gmail.com>
2020-02-11 14:31:56 -0800

    transfer inline styles into objects

--- src/components/Projects/ProjectCard.js
+++ src/components/Projects/ProjectCard.js
@@ -1,24 +1,9 @@
 import React from "react"
-
-const test = {
-    border: "2px solid lightgrey",
-    borderRadius: "5px",
-    boxShadow: "5px 5px 10px lightgrey",
-    position: "relative"
-}
-const flex = {
-    display: "flex", justifyContent: "space-between"
-}
-const hoverLink = {
-    backgroundColor: "lightgrey",
-    padding: "5px 10px",
-    textTransform: "capitalize",
-    color: "black"
-}
+import styles from './styles'
 
 const ProjectCard = (item) => {
     return (
-        <div style={test}>
+        <div style={projectCard}>
             <div style={flex}>
 
                 {item.url ?
@@ -34,52 +19,65 @@ const ProjectCard = (item) => {
                     : null}
             </div>
             <img src={item.img}
-                style={{
-                    display: "block",
-                    height: "300px",
-                    backgroundSize: "cover",
-                    backgroundPosition: "center",
-                    backgroundRepeat: "no-repeat",
-                    overflow: "hidden",
-                    position: "relative"
-                }}
+                style={imgStyle}
                 alt="project screenshot" />
 
             <div style={{ margin: "1rem" }}>
                 <h3>{item.title}</h3>
                 <p>{item.desc}</p>
-                <ul
-                    style={{
-                        display: "flex",
-                        // position: "absolute",
-                        listStyle: "none"
-                    }}>
-                    <li style={{
-                        // backgroundColor: "lightblue",
-                        border: "3px solid lightblue",
-                        borderRadius: "5px",
-                        padding: "5px 10px",
-                        margin: "1rem 0.25rem 1rem 0"
-                    }}>
+                <ul style={ulStyle}>
+                    <li style={tagsStyle}>
                         {item.tags.map(item => {
                             return (
                                 <p>{item.tags}</p>
                             )
                         })}
                     </li>
-                    <li style={{
-                        // backgroundColor: "lightblue",
-                        border: "3px solid lightblue",
-                        borderRadius: "5px",
-                        padding: "5px 10px",
-                        margin: "1rem 0.25rem"
-                    }}>
+                    <li style={tagsStyle}>
                         <p>{"item.tags"}</p>
                     </li>
                 </ul>
             </div>
-        </div >
+        </div>
     )
+}
+
+const projectCard = {
+    border: "2px solid lightgrey",
+    borderRadius: "5px",
+    boxShadow: "5px 5px 10px lightgrey",
+    position: "relative"
+}
+const flex = {
+    display: "flex",
+    justifyContent: "space-between"
+}
+const hoverLink = {
+    backgroundColor: "lightgrey",
+    padding: "5px 10px",
+    textTransform: "capitalize",
+    color: "black"
+}
+const imgStyle = {
+    display: "block",
+    height: "300px",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    overflow: "hidden",
+    position: "relative"
+}
+const ulStyle = {
+    display: "flex",
+    // position: "absolute",
+    listStyle: "none"
+}
+const tagsStyle = {
+    // backgroundColor: "lightblue",
+    border: "3px solid lightblue",
+    borderRadius: "5px",
+    padding: "5px 10px",
+    margin: "1rem 0.25rem 1rem 0"
 }
 
 export default ProjectCard
