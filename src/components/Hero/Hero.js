07b3631fd746c5635b4fa9f248e0a19e700a930b
Fiona <fyeung21@gmail.com>
2020-02-11 15:32:29 -0800

    add fonts

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -2,40 +2,25 @@ import React from "react"
 
 const Hero = () => {
     return (
-        <div style={{ padding: "11rem 0", maxWidth: "70%" }}>
-            <h1>Hi there! I'm Fiona Yeung, a full stack developer from Vancouver, BC.</h1>
-            <ul style={{ display: "flex", listStyle: "none" }}>
-                <li>
-                    <a href="#projects"
-                        style={{
-                            margin: "0 5px",
-                            textTransform: "capitalize",
-                            textDecoration: "none",
-                            color: "black",
-                            border: "2px solid lightblue",
-                            borderRadius: "35px",
-                            padding: "10px 20px"
-                        }}>
-                        projects
-                    </a>
-                </li>
-                <li>
-                    <a href="../../images/resume.pdf"
-                        style={{
-                            margin: "0 5px",
-                            textTransform: "capitalize",
-                            textDecoration: "none",
-                            color: "black",
-                            border: "2px solid lightblue",
-                            borderRadius: "35px",
-                            padding: "10px 20px"
-                        }}>
-                        resume
-                    </a>
-                </li>
-            </ul>
-        </div >
+        <div style={heroStyle}>
+            <div style={borderBox}>
+                <h1>Fiona Yeung</h1>
+                <h2>Full-Stack Developer</h2>
+            </div>
+        </div>
     )
+}
+
+const heroStyle = {
+    padding: '11rem 0'
+}
+
+const borderBox = {
+    padding: '20px 10px',
+    margin: '0 auto',
+    width: '50%',
+    border: '1px solid lightgrey',
+    textAlign: 'center'
 }
 
 export default Hero
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,4 +1,5 @@
 import React from "react"
+import './reset.css'
 import "./layout.css"
 import Header from "./Header/Header"
 import Footer from "./Footer"
