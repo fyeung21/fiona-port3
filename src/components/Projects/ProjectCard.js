c4364d3b8b2873ff97f772ed71ce3fa01b92d441
Fiona <fyeung21@gmail.com>
2020-02-16 05:04:17 -0800

    refactor card style and code

--- src/components/Projects/ProjectCard.js
+++ src/components/Projects/ProjectCard.js
@@ -1,83 +1,113 @@
-import React from "react"
-import styles from './styles'
+import React from "react";
+import styled from "styled-components";
 
 const ProjectCard = (item) => {
+
+    const stack = item.tags;
+
     return (
-        <div style={projectCard}>
-            <div style={flex}>
-
-                {item.url ?
-                    <a href={item.url} style={{ textDecoration: "none" }}>
-                        <p style={hoverLink}>View Site</p>
-                    </a>
-                    : null}
-
-                {item.github ?
-                    <a href={item.github} style={{ textDecoration: "none" }}>
-                        <p style={hoverLink}>View github</p>
-                    </a>
-                    : null}
-            </div>
-            <img src={item.img}
-                style={imgStyle}
+        <StyledCard>
+            <StyledImg
+                src={item.img}
                 alt="project screenshot" />
-
-            <div style={{ margin: "1rem" }}>
-                <h3>{item.title}</h3>
-                <p>{item.desc}</p>
-                <ul style={ulStyle}>
-                    <li style={tagsStyle}>
-                        {item.tags.map(item => {
+            <CardText>
+                <TitleFlex>
+                    <Heading>{item.title}</Heading>
+                    <StackList>
+                        {stack.map(item => {
                             return (
-                                <p>{item.tags}</p>
+                                <StackItem>{item}</StackItem>
                             )
                         })}
-                    </li>
-                    <li style={tagsStyle}>
-                        <p>{"item.tags"}</p>
-                    </li>
-                </ul>
-            </div>
-        </div>
+                    </StackList>
+                </TitleFlex>
+
+                <Text>{item.desc}</Text>
+
+                <Flex>
+                    {item.github ?
+                        <StyledLink href={item.github}>
+                            Source
+                        </StyledLink>
+                        : null
+                    }
+
+                    {item.url ?
+                        <StyledLink href={item.url}>
+                            Demo
+                        </StyledLink>
+                        : null
+                    }
+                </Flex>
+
+            </CardText>
+        </StyledCard>
     )
 }
 
-const projectCard = {
-    border: "2px solid lightgrey",
-    borderRadius: "5px",
-    boxShadow: "5px 5px 10px lightgrey",
-    position: "relative"
-}
-const flex = {
-    display: "flex",
-    justifyContent: "space-between"
-}
-const hoverLink = {
-    backgroundColor: "lightgrey",
-    padding: "5px 10px",
-    textTransform: "capitalize",
-    color: "black"
-}
-const imgStyle = {
-    display: "block",
-    height: "300px",
-    backgroundSize: "cover",
-    backgroundPosition: "center",
-    backgroundRepeat: "no-repeat",
-    overflow: "hidden",
-    position: "relative"
-}
-const ulStyle = {
-    display: "flex",
-    // position: "absolute",
-    listStyle: "none"
-}
-const tagsStyle = {
-    // backgroundColor: "lightblue",
-    border: "3px solid lightblue",
-    borderRadius: "5px",
-    padding: "5px 10px",
-    margin: "1rem 0.25rem 1rem 0"
-}
+const StyledCard = styled.div`
+    border-radius: 5px;
+    box-shadow: 5px 5px 10px lightgrey;
+    position: relative;
+`
+
+const StyledImg = styled.img`
+    display: block;
+    height: 300px;
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    overflow: hidden;
+    position: relative;
+`
+////////////////
+
+const CardText = styled.div`
+    margin: 1rem;
+`
+
+const TitleFlex = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`
+
+const Heading = styled.h2`
+    font-size: 28px;
+    margin: 0.5rem 0;
+`
+
+const StackList = styled.ul`
+    display: flex;
+    list-style: none;
+`
+
+const StackItem = styled.li`
+    margin: 0 0.5rem 0 0;
+    font-style: italic;
+`
+////////////////
+
+const Text = styled.p`
+    font-size: 18px;
+    line-height: 26px;
+`
+
+const Flex = styled.div`
+    display: flex;
+`
+
+const StyledLink = styled.a`
+    text-transform: capitalize;
+    color: black;
+    text-decoration: none;
+    border: 3px solid lightblue;
+    border-radius: 5px;
+    padding: 5px 10px;
+    margin: 1rem 1rem 1rem 0;
+    &:hover {
+        background-color: lightblue;
+    }
+`
 
 export default ProjectCard
--- src/components/Projects/ProjectsContainer.js
+++ src/components/Projects/ProjectsContainer.js
@@ -1,12 +1,13 @@
-import React from "react"
-import ProjectCard from "./ProjectCard"
+import React from "react";
+import ProjectCard from "./ProjectCard";
+import styled from "styled-components";
 
 let projectsList = [
     {
         id: 0,
         title: "Coop Radio",
         desc: "Custom theme for Vancouver Coop Radio's landing site for artists.",
-        img: "../../assets/images/coop.png",
+        img: require("../../assets/images/coop.png"),
         url: "https://radio.van.cp.academy.red",
         github: "https://github.com/fyeung21/radio-ws1-2019",
         tags: ["Php", "JS", "WordPress"]
@@ -16,7 +17,7 @@ let projectsList = [
         id: 1,
         title: "Quotes on Dev",
         desc: "A simple site that cycles through an archive of quotes from devs on click.",
-        img: "../../assets/images/qod.png",
+        img: require("../../assets/images/qod.png"),
         url: null,
         github: "https://github.com/fyeung21/quotes-on-dev",
         tags: ["Ajax", "JS", "Php", "WordPress"]
@@ -26,7 +27,7 @@ let projectsList = [
         id: 2,
         title: "Inhabitent",
         desc: "A fictional camping supply website, complete with a blog and shop.",
-        img: "../../assets/images/inhabit.png",
+        img: require("../../assets/images/inhabit.png"),
         url: null,
         github: "https://github.com/fyeung21/Inhabitent-Site",
         tags: ["Php", "JS", "WordPress"]
@@ -36,7 +37,7 @@ let projectsList = [
         id: 3,
         title: "Instanews",
         desc: "A simple website that uses ajax to pull top stories from the New York Times.",
-        img: "../../assets/images/instanews.png",
+        img: require("../../assets/images/instanews.png"),
         url: "https://github.com/fyeung21/Instanews-App",
         github: "https://github.com/fyeung21/Instanews-App",
         tags: ["Ajax", "CSS", "JS"]
@@ -46,7 +47,7 @@ let projectsList = [
         id: 4,
         title: "Pong",
         desc: "The classic pong game using js and svgs with special game features.",
-        img: "../../assets/images/pong.png",
+        img: require("../../assets/images/pong.png"),
         url: "https://fyeung21.github.io/pong-project/",
         github: "https://github.com/fyeung21/pong-project",
         tags: ["JS", "SVGs", "Node"]
@@ -56,7 +57,7 @@ let projectsList = [
         id: 5,
         title: "Aloha Apparel",
         desc: "A fictional clothing retail webpage. Utilized flickity.js plugin.",
-        img: "../../assets/images/aloha.png",
+        img: require("../../assets/images/aloha.png"),
         url: "https://fyeung21.github.io/Aloha-Webpage/",
         github: "https://github.com/fyeung21/Aloha-Webpage",
         tags: ["HTML", "CSS", "JS"]
@@ -68,13 +69,8 @@ const ProjectsContainer = () => {
 
     return (
         <div>
-            <h1 style={{ textTransform: "capitalize", textAlign: "center", marginTop: "5rem" }}>projects</h1>
-            <div style={{
-                display: "grid",
-                gridTemplateColumns: "repeat(3, 1fr)",
-                gridGap: "6.5rem 4.5rem",
-                margin: "5rem auto"
-            }}>
+            <SectionHeading>projects</SectionHeading>
+            <StyledGrid>
                 {projectsList.map(item => {
                     return (
                         <ProjectCard
@@ -87,9 +83,22 @@ const ProjectsContainer = () => {
                         />
                     )
                 })}
-            </div>
+            </StyledGrid>
         </div>
     )
 }
+
+const SectionHeading = styled.h1`
+    text-transform: capitalize;
+    text-align: center;
+    margin-top: 5rem;
+    font-size: 40px;
+`
+const StyledGrid = styled.div`
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-gap: 6.5rem 4.5rem;
+    margin: 5rem auto;
+`
 
 export default ProjectsContainer
