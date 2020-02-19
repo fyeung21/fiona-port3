a7eb86fae79820ac4d7abd691cc4fd376285000f
Fiona <fyeung21@gmail.com>
2020-02-19 05:24:23 -0800

    move data to separate files

--- src/components/MoreProjects/MoreProjectsCont.js
+++ src/components/MoreProjects/MoreProjectsCont.js
@@ -2,66 +2,10 @@ import React from "react";
 import ProjectCard from "../Projects/ProjectCard";
 import styled from "styled-components";
 import { Link } from "gatsby";
-
-let projectsList = [
-    {
-        id: 0,
-        title: "Aloha Apparel",
-        desc: "A fictional clothing retail webpage. Utilized flickity.js plugin.",
-        img: require("../../assets/images/aloha.png"),
-        url: "https://fyeung21.github.io/Aloha-Webpage/",
-        github: "https://github.com/fyeung21/Aloha-Webpage",
-        tags: ["HTML", "CSS", "JS"]
-    },
-    {
-        id: 1,
-        title: "Pong",
-        desc: "The classic pong game using js and svgs with special game features.",
-        img: require("../../assets/images/pong.png"),
-        url: "https://fyeung21.github.io/pong-project/",
-        github: "https://github.com/fyeung21/pong-project",
-        tags: ["JS", "SVGs", "Node"]
-    },
-    {
-        id: 2,
-        title: "Instanews",
-        desc: "A simple website that uses ajax to pull top stories from the New York Times.",
-        img: require("../../assets/images/instanews.png"),
-        url: "https://github.com/fyeung21/Instanews-App",
-        github: "https://github.com/fyeung21/Instanews-App",
-        tags: ["Ajax", "CSS", "JS"]
-    },
-    {
-        id: 3,
-        title: "Inhabitent",
-        desc: "A fictional camping supply website, complete with a blog and shop.",
-        img: require("../../assets/images/inhabit.png"),
-        url: null,
-        github: "https://github.com/fyeung21/Inhabitent-Site",
-        tags: ["Php", "JS", "WordPress"]
-    },
-    {
-        id: 4,
-        title: "Quotes on Dev",
-        desc: "A simple site that cycles through an archive of quotes from devs on click.",
-        img: require("../../assets/images/qod.png"),
-        url: null,
-        github: "https://github.com/fyeung21/quotes-on-dev",
-        tags: ["Ajax", "JS", "Php", "WordPress"]
-    },
-    {
-        id: 5,
-        title: "Coop Radio",
-        desc: "Custom theme for Vancouver Coop Radio's landing site for artists.",
-        img: require("../../assets/images/coop.png"),
-        url: "https://radio.van.cp.academy.red",
-        github: "https://github.com/fyeung21/radio-ws1-2019",
-        tags: ["Php", "JS", "WordPress"]
-    }
-];
+import moreProjectsList from "./moreList";
 
 const MoreProjectsCont = () => {
-    const reverseList = projectsList.reverse()
+    const reverseList = moreProjectsList.reverse()
 
     return (
         <div>
---
+++ src/components/MoreProjects/moreList.js
@@ -0,0 +1,85 @@
+const moreProjectsList = [
+    {
+        id: 0,
+        title: "Aloha Apparel",
+        desc: "A fictional clothing retail webpage. Utilized flickity.js plugin.",
+        img: require("../../assets/images/aloha.png"),
+        url: "https://fyeung21.github.io/Aloha-Webpage/",
+        github: "https://github.com/fyeung21/Aloha-Webpage",
+        tags: ["HTML", "CSS", "JS"]
+    },
+    {
+        id: 1,
+        title: "Pong",
+        desc: "The classic pong game using js and svgs with special game features.",
+        img: require("../../assets/images/pong.png"),
+        url: "https://fyeung21.github.io/pong-project/",
+        github: "https://github.com/fyeung21/pong-project",
+        tags: ["JS", "NodeJS"]
+    },
+    {
+        id: 2,
+        title: "Instanews",
+        desc: "A simple website that uses ajax to pull top stories from the New York Times.",
+        img: require("../../assets/images/instanews.png"),
+        url: "https://github.com/fyeung21/Instanews-App",
+        github: "https://github.com/fyeung21/Instanews-App",
+        tags: ["CSS", "JS"]
+    },
+    {
+        id: 3,
+        title: "Inhabitent",
+        desc: "A fictional camping supply website, complete with a blog and shop.",
+        img: require("../../assets/images/inhabit.png"),
+        url: null,
+        github: "https://github.com/fyeung21/Inhabitent-Site",
+        tags: ["CSS", "JS", "WordPress"]
+    },
+    {
+        id: 4,
+        title: "Quotes on Dev",
+        desc: "A simple site that cycles through an archive of quotes from devs on click.",
+        img: require("../../assets/images/qod.png"),
+        url: null,
+        github: "https://github.com/fyeung21/quotes-on-dev",
+        tags: ["CSS", "JS", "WordPress"]
+    },
+    {
+        id: 5,
+        title: "Coop Radio",
+        desc: "Custom theme for Vancouver Coop Radio's landing site for artists.",
+        img: require("../../assets/images/coop.png"),
+        url: "https://radio.van.cp.academy.red",
+        github: "https://github.com/fyeung21/radio-ws1-2019",
+        tags: ["CSS", "JS", "WordPress"]
+    },
+    {
+        id: 6,
+        title: "Boomtown",
+        desc: "Sharing and borrowing site",
+        img: require("../../assets/images/qod.png"),
+        url: null,
+        github: "https://github.com/fyeung21/boomtown",
+        tags: ["React", "Apollo Client", "GraphQL"]
+    },
+    {
+        id: 7,
+        title: "Self-Order",
+        desc: "self ordering dimsum web app",
+        img: require("../../assets/images/pong.png"),
+        url: null,
+        github: "https://github.com/fyeung21/self-order",
+        tags: ["React", "Meteor", "MongoDB"]
+    },
+    {
+        id: 8,
+        title: "R10",
+        desc: "Mobile App for a fictional conference event",
+        img: require("../../assets/images/pong.png"),
+        url: null,
+        github: "https://github.com/fyeung21/r10-app",
+        tags: ["React Native", "Apollo Client", "GraphQL"]
+    }
+];
+
+export default moreProjectsList;
