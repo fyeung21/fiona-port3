69db80bf92ba291b44b937bc4c7c0c1311b95081
Fiona <fyeung21@gmail.com>
2020-02-22 03:14:50 -0800

    edit about me img styling

--- src/pages/about-me.js
+++ src/pages/about-me.js
@@ -11,7 +11,7 @@ const AboutMe = () => (
       <SectionHeading>about me</SectionHeading>
       <Flex>
         <AboutBio />
-        <ProfileImg src={require("../assets/images/me-fullbody.jpg")} alt="me" />
+        <ProfileImg src={require("../assets/images/me-fullbody.jpg")} alt="picture-of-me" />
       </Flex>
     </>
   </Layout>
@@ -20,17 +20,18 @@ const AboutMe = () => (
 const SectionHeading = styled.h1`
     text-transform: capitalize;
     text-align: center;
-    margin-top: 5rem;
+    margin: 5rem 0;
     font-size: 40px;
 `
 const Flex = styled.div`
     display: flex;
-    margin: 3rem 0;
+    margin: 3rem 5rem;
 `
 const ProfileImg = styled.img`
-    height: 500px;
+    height: 600px;
     border-radius: 5px;
     box-shadow: 5px 5px 10px lightgrey;
+    margin: 0 0 5rem 8rem;
 `
 
 export default AboutMe
