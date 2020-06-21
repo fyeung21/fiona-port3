340162f01368ae6d4d26812348db557072ea9b3c
Fiona <fyeung21@gmail.com>
2020-06-20 23:12:25 -0700

    remove comments

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,19 +2,15 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-// import Hero from "../components/Hero/Hero"
 import MainProjectsContainer from "../components/MainProjects/MainProjectsCont"
 import SkillsContainer from "../components/Skills/SkillsContainer"
-// import Resume from "../components/Resume/Resume"
 
 const Portfolio = () => (
   <Layout>
     <SEO title="Home" />
     <div>
-      {/* <Hero /> */}
       <MainProjectsContainer />
       <SkillsContainer />
-      {/* <Resume /> */}
     </div>
   </Layout>
 )
