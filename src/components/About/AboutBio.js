449333483fd93f40ac0ee964aed4207fed055366
Fiona <fyeung21@gmail.com>
2020-07-03 23:50:00 -0700

    add location in about me

--- src/components/About/AboutBio.js
+++ src/components/About/AboutBio.js
@@ -1,15 +1,16 @@
 import React from "react";
-import { TextCont, Bio } from "./aboutStyles";
+import { TextCont, Location, Bio } from "./aboutStyles";
 
 const AboutBio = () => {
     return (
         <TextCont>
+            <Location>Location: Vancouver, BC</Location>
             <Bio>
                 Forever a student in life and it’s subjects,  I've eventually found myself stumbling upon web
                 and app development. This opened me up to not only the world of computer languages, but also
                 the vibrant and innovative community it comes with. I enjoy building websites and applications
                 because it’s an interactive art. Seeing users interact with what I built satisfies me greatly and
-                motivates me to learn how to create better and more efficient applications. I am an easy going
+                motivates me to learn how to create better and more efficient applications. I am very passionate about collaborative work and put emphasis on communication and writing clear code. I am an easy going
                 individual who is very goal oriented and strives to get things done while acknowledging that
                 there is always room for improvement.
             </Bio>
--- src/components/About/aboutStyles.js
+++ src/components/About/aboutStyles.js
@@ -17,13 +17,23 @@ export const TextCont = styled.div`
         max-width: 80%;
     }
 `
+export const Location = styled.p`
+    font-family: ${theme.bodyFont};
+    font-size: ${theme.bodyFontSize};
+    color: ${({ theme }) => theme.mainTxtColor};
+    width: 90%;
+    margin: 3rem auto 3rem;
+    text-align: center;
+    line-height: 32px;
+`
+
 export const Bio = styled.p`
     font-family: ${theme.bodyFont};
     font-size: ${theme.bodyFontSize};
     color: ${({ theme }) => theme.mainTxtColor};
+    width: 90%;
+    margin: 0 auto;
     text-align: left;
+    text-indent: 50px;
     line-height: 32px;
-    @media (min-width: ${theme.tabletSize}) {
-        text-align: center;
-    }
 `
