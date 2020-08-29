adb71a0fa8db45ab52e905235ffaa74262d7771e
Fiona <fyeung21@gmail.com>
2020-08-28 22:01:52 -0700

    refactor: Prepare stats component dynamic

--- src/components/ProjectDetails/Stats.js
+++ src/components/ProjectDetails/Stats.js
@@ -1,40 +1,49 @@
 import React from "react";
-import { StatsMainCont, ProjectTitle, BriefCont, SubFlexCont, TypeCont, Type, Txt, TxtLink } from "./detailStyles";
 import { Container } from "../globalStyles";
+import { StatsMainCont, ProjectTitle, BriefCont, SubFlexCont, TypeCont, Type, Txt, TxtLink } from "./detailStyles";
 
-const Stats = () => {
+const Stats = ({ title, brief, platform, team, role, demo, github, tech }) => {
     return (
         <Container>
             <StatsMainCont>
-                <ProjectTitle>{"Project Title"}</ProjectTitle>
+                <ProjectTitle>{title}</ProjectTitle>
                 <BriefCont>
                     <Type>brief</Type>
-                    <Txt>{"There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable."}</Txt>
+                    <Txt>{brief}</Txt>
                 </BriefCont>
                 <SubFlexCont>
                     <TypeCont>
                         <Type>platform</Type>
-                        <Txt>{"Web/Mobile"}</Txt>
+                        <Txt>{platform}</Txt>
                     </TypeCont>
                     <TypeCont>
                         <Type>team</Type>
-                        <Txt>{"Solo"}</Txt>
+                        <Txt>{team}</Txt>
                     </TypeCont>
                     <TypeCont>
                         <Type>role</Type>
-                        <Txt>{"Developer"}</Txt>
+                        <Txt>{role}</Txt>
                     </TypeCont>
-                    <TypeCont>
-                        <Type>status</Type>
-                        <Txt>{"Not deployed"}</Txt>
-                    </TypeCont>
+
+                    {demo ?
+                        <TypeCont>
+                            <Type>status</Type>
+                            <TxtLink href={demo} target="_blank" rel="noopener noreferrer">Active</TxtLink>
+                        </TypeCont>
+                        :
+                        <TypeCont>
+                            <Type>status</Type>
+                            <Txt>{"Not deployed"}</Txt>
+                        </TypeCont>
+                    }
+
                     <TypeCont>
                         <Type>source</Type>
-                        <TxtLink href="https://github.com/fyeung21" target="_blank" rel="noopener noreferrer">Link</TxtLink>
+                        <TxtLink href={github} target="_blank" rel="noopener noreferrer">Link</TxtLink>
                     </TypeCont>
                     <TypeCont>
                         <Type>technologies</Type>
-                        <Txt>{"React, GraphQLReact, GraphQLReact, GraphQL"}</Txt>
+                        <Txt>{tech}</Txt>
                     </TypeCont>
                 </SubFlexCont>
             </StatsMainCont>
