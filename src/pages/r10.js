7c78b605152f320615f351ac4689053314040144
Fiona <fyeung21@gmail.com>
2020-08-28 20:57:52 -0700

    feat: Add new project pages

---
+++ src/pages/r10.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+
+import Banner from "../components/ProjectDetails/Banner";
+import Stats from "../components/ProjectDetails/Stats";
+import ProjectImg from "../components/ProjectDetails/ProjectImg";
+import ProjectNav from "../components/ProjectDetails/ProjectNav";
+
+const ProjectDetails = () => (
+    <Layout>
+        <SEO title="R10" />
+        <Banner bannerImg={"https://via.placeholder.com/650x450/d3d3d3"} />
+        <Stats />
+        <ProjectImg projectImg={"https://via.placeholder.com/650x450/d3d3d3"} />
+        <ProjectImg projectImg={"https://via.placeholder.com/650x450/d3d3d3"} />
+        <ProjectImg projectImg={"https://via.placeholder.com/650x450/d3d3d3"} />
+        <ProjectNav />
+    </Layout>
+)
+
+export default ProjectDetails;
